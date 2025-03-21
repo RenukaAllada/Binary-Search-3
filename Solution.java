9a030d926f4998210c2fed4d21e2a5509942909f
Renuka Allada <renuka@Sureedus-Air.attlocal.net>
2025-03-21 07:28:31 -0500

    completed 2 problems

--- Solution.java
+++ Solution.java
@@ -20,6 +20,32 @@ class Solution{
         }
     }
 
+    //TC: 0(log N)
+//SC: 0(1)
+    class Solution {
+        public List<Integer> findClosestElements(int[] arr, int k, int x) {
+            if(arr.length==0 || arr==null || k==0){
+                return new ArrayList<>();
+            }
+            int low=0,high=arr.length-k;
+            List<Integer> result=new ArrayList<>();
+            while(low<high){
+                int mid=low+(high-low)/2;
+                int startDistance=x-arr[mid];
+                int endDistance=arr[mid+k]-x;
+                if(startDistance>endDistance){
+                    low=mid+1;
+                }else{
+                    high=mid;
+                }
+            }
+
+            for(int i=low;i<low+k;i++){
+                result.add(arr[i]);
+            }
+            return result;
+        }
+    }
     
     /*****************problem-2*********************/
 
